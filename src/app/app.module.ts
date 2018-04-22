d33c5dbdfeca3e1298bdd9538f9bf017b45134d6
Deymer Jimenez Lora <dei1562@outlook.com>
2018-04-22 12:17:59 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCdgb4Gyn7pFDCahbOV4AcDhAr4hSXVnAE",
+      authDomain: "concesionario-e58ea.firebaseapp.com",
+      databaseURL: "https://concesionario-e58ea.firebaseio.com",
+      projectId: "concesionario-e58ea",
+      storageBucket: "concesionario-e58ea.appspot.com",
+      messagingSenderId: "179035316970"
+    }),
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp  
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
